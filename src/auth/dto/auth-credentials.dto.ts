cde482ba3a532ea1de1a2fb6638724beec4cd1a9
impressiveHen <b02505002@gmail.com>
2020-09-27 16:05:17 -0700

    add swagger documentation
    Signed-off-by: impressiveHen <b02505002@gmail.com>

--- src/auth/dto/auth-credentials.dto.ts
+++ src/auth/dto/auth-credentials.dto.ts
@@ -1,11 +1,14 @@
 import { IsString, MaxLength, MinLength } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class AuthCredentialsDto {
+    @ApiProperty()
     @IsString()
     @MinLength(4)
     @MaxLength(20)
     username: string;
 
+    @ApiProperty()
     @IsString()
     @MinLength(4, { message: 'Password should be longer than 4 characters'})
     @MaxLength(20, { message: 'Password should be shorter than 20 characters'})
